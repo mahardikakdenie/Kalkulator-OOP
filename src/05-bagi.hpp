04645ba2fe48eaca85b74eb55261c88278872635
mahardika denie <dikamahar884@gmail.com>
2020-12-24 16:47:39 +0700

    float

--- src/05-bagi.hpp
+++ src/05-bagi.hpp
@@ -10,17 +10,17 @@ using namespace std;
 
 class Bagi : public Kalkulator{
     private:
-       
+       float bil3,bil4;
     public:
         void Pembagian(){
             cout<<"\n[/] Pembagian "<<endl;
-            cout<<"Inputan 1 : ";cin>>bil1;
-            cout<<"Inputan 2 : ";cin>>bil2;
+            cout<<"Inputan 1 : ";cin>>bil3;
+            cout<<"Inputan 2 : ";cin>>bil4;
         }
         void Display(){
             cout<<"\n[/] Hasil Pembagian "<<endl;
-            Hasil = bil1 / bil2 ;
-            cout<<"Hasil dari "<<bil1<<" / "<<bil2<<" = " <<Hasil<<endl;
+            Hasil = bil3 / bil4;
+            cout<<"Hasil dari "<<bil3<<" / "<<bil4<<" = " <<Hasil<<endl;
         }
 };
 
